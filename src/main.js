9c98a544d2719ddb333583ad07ea56f5440f5bf9
1053061407 <1053061407@qq.com>
2018-02-14 17:11:48 +0800

    删除无用代码

--- src/main.js
+++ src/main.js
@@ -7,14 +7,10 @@ import router from './router'
 import store from './store'
 import '@/icons' // icon
 import '@/permission' // 权限
-import * as filters from './filters' // 全局filter
 import './assets/iconfont'
 
 Vue.use(ElementUI, { locale })
 
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
 
 Vue.config.productionTip = false
 
